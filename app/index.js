82ae04817b0ed6a68f07d4871d1fc014881a84f9
romesdev <fs.romes@gmail.com>
2023-10-18 16:45:37 -0300

    feat: first commit with starting and stopping of the listing working

---
+++ app/index.js
@@ -0,0 +1,81 @@
+const API_URL = "http://localhost:3000";
+
+async function consumeAPI(signal) {
+  const response = await fetch(API_URL, {
+    signal,
+  });
+  let counter = 0;
+  const reader = response.body
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(parseNDJSON());
+  // .pipeTo(
+  //   new WritableStream({
+  //     write(chunk) {
+  //       console.log(++counter, "chunk", chunk);
+  //     },
+  //   })
+  // );
+
+  return reader;
+}
+
+function appendToHTML(element) {
+  return new WritableStream({
+    write({ title, description, url_anime }) {
+      const card = `
+      <article>
+      <div class="text">
+        <h3>${title}</h3>
+        <p>
+        ${description.slice(0, 100)}
+        </p>
+        <a href="${url_anime}">See more...</a>
+      </div>
+    </article>
+    `;
+      element.innerHTML += card;
+    },
+    abort(reason) {
+      console.log("aborted", reason);
+    },
+  });
+}
+
+function parseNDJSON() {
+  let ndjsonBuffer = "";
+  return new TransformStream({
+    transform(chunk, controller) {
+      ndjsonBuffer += chunk;
+      const items = ndjsonBuffer.split("\n");
+      items
+        .slice(0, -1)
+        .forEach((item) => controller.enqueue(JSON.parse(item)));
+
+      ndjsonBuffer = items[items.length - 1];
+    },
+    flush(controller) {
+      if (!ndjsonBuffer) return;
+      controller.enqueue(JSON.parse(ndjsonBuffer));
+    },
+  });
+}
+
+const [start, stop, cards] = ["start", "stop", "cards"].map((item) =>
+  document.getElementById(item)
+);
+
+let abortController = new AbortController();
+
+start.addEventListener("click", async () => {
+  try {
+    const readable = await consumeAPI(abortController.signal);
+    readable.pipeTo(appendToHTML(cards), { signal: abortController.signal });
+  } catch (error) {
+    if (!error.message.includes("abort")) throw error;
+  }
+});
+
+stop.addEventListener("click", async () => {
+  abortController.abort();
+  abortController = new AbortController();
+});
